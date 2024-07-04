2e7c3f6a59dc2ed1b189b1c0d961e11c6e211945
timmychan823 <chanshunhei09@gmail.com>
2024-07-04 22:33:58 +0800

    changed linux command

--- src/EmailReminder.java
+++ src/EmailReminder.java
@@ -24,24 +24,18 @@ public class EmailReminder {
 
             FileWriter writer = new FileWriter("latestRecord.txt",false);
             writer.write("Subject: Test\nTo: tshchan@hkma.gov.hk\nCc: chanshunhei09@gmail.com\n\n");
-            System.out.println("Meta-data added");
             writer.write("The following accounts will expire soon:\n");
-            System.out.println("expired noti added");
-
             writer.write(String.format("%15s%15s%15s","Column1","Column2","Column3")+"\n"); //change to correct column names later
-            System.out.println("columns added");
             writer.write(String.join("", Collections.nCopies(45, "-"))+"\n"); //change to correct length later
             while (rs.next()) {
-
                 writer.write(String.format("%15s%15s%15s",rs.getString(1), rs.getString(2), rs.getString(3))+"\n"); //change this later to fit selection result
-                System.out.println(String.format("%15s%15s%15s",rs.getString(1), rs.getString(2), rs.getString(3))+"\n");
             }
             // step5 close the connection object
 
             con.close();
             writer.close();
 
-            Process proc = Runtime.getRuntime().exec("sendmail -i -t <latestRecord.txt");
+            Process proc = Runtime.getRuntime().exec("sendmail -i -t \"tshchan@hkma.gov.hk,chanshunhei09@gmail.com\" <latestRecord.txt");
 
         } catch (Exception e) {
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
