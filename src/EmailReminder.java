8294ee2ca86d99f7a3e7ff17ce1b2e7ba7bbfef4
timmychan823 <chanshunhei09@gmail.com>
2024-07-16 15:37:23 +0800

    changed linux command

--- src/EmailReminder.java
+++ src/EmailReminder.java
@@ -23,6 +23,8 @@ public class EmailReminder {
             String configuration="";
             String salt = "MySalt";
             String secretKey="";
+            int no_of_rows = 0;
+            String warning_heading = "[CLEAR]";
 
 
             fin = new FileInputStream("./configuration/configuration (encrypted).txt");
@@ -57,13 +59,21 @@ public class EmailReminder {
                     ResultSet.CONCUR_READ_ONLY);
             ResultSet rs = st.executeQuery(sql);
 
-            //int[] columnLengths= EmailReminder.maxColumns(rs);
+            if (rs.last()){
+                 no_of_rows = rs.getRow();
+            }
+
             rs.beforeFirst();
 
             FileWriter writer = new FileWriter("latestRecord.txt",false);
-            writer.write("Subject: Password Expiry Reminder\nTo: "+toAddress+"\nContent-Type: text/html; charset=\"UTF-8\"\n\n");
+
+            if (no_of_rows>0){
+                warning_heading = "[WARNING]";
+            }
+
+            writer.write("Subject: [DBA][SYSADM] Password Expiry Reminder "+ warning_heading+"\nTo: "+toAddress+"\nContent-Type: text/html; charset=\"UTF-8\"\n\n");
             writer.write("The following accounts' passwords will expire soon:\n");
-            writer.write(String.format("<table border=\"1\"><tr><th>%s</th><th>%s</th><th>%s</th></tr>","id","Number of days before expiry","Expiry date")+"\n"); //change to correct column names later
+            writer.write(String.format("<table border=\"1\"><tr><th>%s</th><th>%s</th><th>%s</th></tr>","USER_ID","Number of days before expiry","Expiry date")+"\n"); //change to correct column names later
             while (rs.next()) {
                 writer.write(String.format("<tr><td>%s</td><td>%s</td><td>%s</td></tr>",rs.getString(1), rs.getString(2), rs.getString(3))+"\n"); //change this later to fit selection result
             }
@@ -89,25 +99,4 @@ public class EmailReminder {
 
     }
 
-    public static int[] maxColumns(ResultSet resultSet) throws SQLException {
-        int columnNumber = resultSet.getMetaData().getColumnCount();
-        int[] max = new int[columnNumber];
-        int currentRow = resultSet.getRow();
-        int fetchDirection = resultSet.getFetchDirection();
-        if (fetchDirection == ResultSet.FETCH_FORWARD) {
-            resultSet.beforeFirst();
-        }else {
-            resultSet.afterLast();
-        }
-
-        while (resultSet.next()) {
-            for (int i = 1; i <= columnNumber; i++) { //Null Pointer Exception
-                if (resultSet.getString(i).length() > max[i-1]) {
-                    max[i - 1] = resultSet.getString(i).length();
-                }
-            }
-        }
-        resultSet.absolute(currentRow);
-        return max;
-    }
 }
