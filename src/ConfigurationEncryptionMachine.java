f91420be825cc6280517761e044c594f8ba55d3b
timmychan823 <chanshunhei09@gmail.com>
2024-07-17 14:03:03 +0800

    changed linux command

--- src/ConfigurationEncryptionMachine.java
+++ src/ConfigurationEncryptionMachine.java
@@ -3,42 +3,33 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Properties;
 import java.util.Random;
 
 public class ConfigurationEncryptionMachine {
 
     public static void main(String[] args) throws Exception {
         // Define your secret key and salt (keep these secure and don't hardcode in production)
-        String secretKey = ConfigurationEncryptionMachine.getRandomKey(); //you should generate a random key if this is an encryption program
-        String salt = "MySalt";
-        FileInputStream fin=null;
-        int ch;
-        String configuration="";
+        String secretKey = ConfigurationEncryptionMachine.getRandomString(20); //you should generate a random key if this is an encryption program
+        String salt = ConfigurationEncryptionMachine.getRandomString(20);
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 
 
         StringWriter sw = new StringWriter();
 
         try{
-            fin = new FileInputStream("./configuration/configuration.txt");
-            if (fin != null) {
-                while ((ch = fin.read()) != -1) {
-                    configuration=configuration+(char)ch;
-                }
-            }
+            InputStream input = new FileInputStream("config.properties");
+            Properties prop = new Properties();
+            prop.load(input);
+            prop.setProperty("db.password",AES256.encrypt(prop.getProperty("db.password"), secretKey, salt));
+            prop.setProperty("encryptionKey",secretKey);
+            prop.setProperty("encryptionSalt",salt);
 
+            System.out.println(prop.getProperty("db.query"));
 
-            // Encrypt the string
-            String encryptedString = AES256.encrypt(configuration, secretKey, salt); //this should be a separate encryption program
+            OutputStream output = new FileOutputStream("config.properties");
+            prop.store(output,null);
 
-            FileOutputStream writer = new FileOutputStream("./configuration/configuration (encrypted).txt");
-            writer.write(encryptedString.getBytes());
-
-            ProcessBuilder pb = new ProcessBuilder();
-            pb.command("/bin/bash","-c","rm ./configuration/configuration.txt"); //replace this to /usr/bin/bash
-            Process process = pb.start();
-            writer = new FileOutputStream("./configuration/secretKey.txt");
-            writer.write(secretKey.getBytes());
         }catch (Exception e){
             Files.createDirectories(Paths.get("./errorLog/encryptionError/"));
 
@@ -57,16 +48,16 @@ public class ConfigurationEncryptionMachine {
 
 
     }
-    private static String getRandomKey() {
-        String randomKeySet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder key = new StringBuilder();
+    private static String getRandomString(int randomStringLength) {
+        String randomCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder randomStringBuilder = new StringBuilder();
         Random rnd = new Random();
-        while (key.length() < 20) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * randomKeySet.length());
-            key.append(randomKeySet.charAt(index));
+        while (randomStringBuilder.length() < randomStringLength) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * randomCharSet.length());
+            randomStringBuilder.append(randomCharSet.charAt(index));
         }
-        String randomKey = key.toString();
-        return randomKey;
+        String randomString = randomStringBuilder.toString();
+        return randomString;
 
     }
 }
--- src/EmailReminder.java
+++ src/EmailReminder.java
@@ -7,6 +7,7 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Properties;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -17,92 +18,86 @@ import java.util.Collections;
 //send every day but 2 types (with expiry and without expiry)
 
 public class EmailReminder {
-    public static void main(String[] args) throws Exception{
+    private static final String EMAIL_SUBJECT = "Subject: [DBA][SYSADM] Password Expiry Reminder ";
+    private static final String CONTENT_TYPE="\nContent-Type: text/html; charset=\"UTF-8\"\n\n";
+    private static final String EMAIL_BODY_WITHOUT_DATA= "The following accounts' passwords will expire soon:\n"+String.format("<table border=\"1\"><tr><th>%s</th><th>%s</th><th>%s</th></tr>","USER_ID","Number of days before expiry","Expiry date")+"\n";
+
+    public static void main(String[] args) throws IOException{
         try{
+            InputStream input = new FileInputStream("config.properties");
+            Properties prop = new Properties();
+            prop.load(input);
 
-            FileInputStream fin=null;
-            int ch;
-            String configuration="";
-            String salt = "MySalt";
-            String secretKey="";
-            int no_of_rows = 0;
+            String salt = prop.getProperty("encryptionSalt");
+            String secretKey= prop.getProperty("encryptionKey");
             String warning_heading = "[CLEAR]";
+            String bash_location = prop.getProperty("bashLocation");
+            String query =prop.getProperty("db.query"); ;
+            String url =prop.getProperty("db.url");;
+            String username =prop.getProperty("db.username");;
+            String password = AES256.decrypt(prop.getProperty("db.password"),secretKey,salt);
+            String toAddress = prop.getProperty("bashLocation");;
+            int no_of_rows = 0;
 
 
-            fin = new FileInputStream("./configuration/configuration (encrypted).txt");
-            if (fin != null) {
-                while ((ch = fin.read()) != -1) {
-                    configuration=configuration+(char)ch;
-                }
-            }
-
-            fin = new FileInputStream("./configuration/secretKey.txt");
-            if (fin != null) {
-                while ((ch = fin.read()) != -1) {
-                    secretKey=secretKey+(char)ch;
-                }
-            }
-
-            String decryptedConfiguration = AES256.decrypt(configuration,secretKey,salt);
-            JSONObject jsonObject = new JSONObject(decryptedConfiguration);
-
-
-
-
-            String sql = jsonObject.getString("query");
-            String url = jsonObject.getString("url");
-            String username = jsonObject.getString("username");
-            String password = jsonObject.getString("password");
-            String toAddress = jsonObject.getString("toAddress");
             Connection con = DriverManager.getConnection(url, username, password);
 
             Statement st = con.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_READ_ONLY);
-            ResultSet rs = st.executeQuery(sql);
+            ResultSet rs = st.executeQuery(query);
 
             if (rs.last()){
                  no_of_rows = rs.getRow();
             }
 
             rs.beforeFirst();
-
-            FileWriter writer = new FileWriter("latestRecord.txt",false);
+            FileWriter writer = new FileWriter("latestRecord.txt", false);
 
             if (no_of_rows>0){
                 warning_heading = "[WARNING]";
             }
 
-            writer.write("Subject: [DBA][SYSADM] Password Expiry Reminder "+ warning_heading+"\nTo: "+toAddress+"\nContent-Type: text/html; charset=\"UTF-8\"\n\n");
-            writer.write("The following accounts' passwords will expire soon:\n");
-            writer.write(String.format("<table border=\"1\"><tr><th>%s</th><th>%s</th><th>%s</th></tr>","USER_ID","Number of days before expiry","Expiry date")+"\n"); //change to correct column names later
+            //fixed part of the email subject and body
+            writer.write(EMAIL_SUBJECT+ warning_heading+"\nTo: "+toAddress+CONTENT_TYPE);
+            writer.write(EMAIL_BODY_WITHOUT_DATA);
+            //email body data part
             while (rs.next()) {
-                writer.write(String.format("<tr><td>%s</td><td>%s</td><td>%s</td></tr>",rs.getString(1), rs.getString(2), rs.getString(3))+"\n"); //change this later to fit selection result
+                writer.write(String.format("<tr><td>%s</td><td>%s</td><td>%s</td></tr>",rs.getString(1), rs.getString(2), rs.getString(3))+"\n");
             }
             writer.write("</table>");
-            // step5 close the connection object
 
             con.close();
             writer.close();
 
             ProcessBuilder pb = new ProcessBuilder();
-            pb.command("/usr/bin/bash","-c","sendmail -i -t \""+toAddress+"\" < latestRecord.txt");
+            pb.command(bash_location,"-c","sendmail -i -t \""+toAddress+"\" < latestRecord.txt");
             Process process = pb.start();
+
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+            Files.createDirectories(Paths.get("./successLog/"));
+            File successLogFile = new File("./successLog/"+timeStamp+"_SuccessLog.txt");
+            if (!successLogFile.exists()){
+                successLogFile.createNewFile();
+            }
+            OutputStream successLogOS = new FileOutputStream(successLogFile);
+            successLogOS.write("Email Successfully Delivered".getBytes());
+
 
         } catch (Exception e) {
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
             Files.createDirectories(Paths.get("./errorLog/forEmailReminder_error/"));
-            File file = new File("./errorLog/forEmailReminder_error/"+timeStamp+"_ErrorLog.txt");
-            if (!file.exists()){
-                file.createNewFile();
+            File errorLogFile = new File("./errorLog/forEmailReminder_error/"+timeStamp+"_ErrorLog.txt");
+            if (!errorLogFile.exists()){
+                errorLogFile.createNewFile();
             }
-            OutputStream os = new FileOutputStream(file);
+            OutputStream errorLogOS = new FileOutputStream(errorLogFile);
 
             StringWriter sw = new StringWriter();
             e.printStackTrace(new PrintWriter(sw));
             String exceptionAsString = sw.toString();
-            os.write(exceptionAsString.getBytes());
-            os.close();
+            errorLogOS.write(exceptionAsString.getBytes());
+            errorLogOS.close();
         }
 
     }
